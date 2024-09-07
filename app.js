47efa34321c545006213a5378c333ab36bfd953f
roxeannechico <roseannechico22@gmail.com>
2024-09-07 22:34:33 +0800

    Initial commit with app.js

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 console.log('Hello World!');
 let name = 'Rose Anne Chico';
 console.log('My name is ' +name);
+
+console.log('Variables');
+let myname = 'Rose Anne Chico';
+let age = '18';
+console.log('I am ' +name+ ', '+age + ' years old. ');
+
+console.log('Numbers');
+const x = 2; 
+const y = 3;
+const sum = x + y;
+console.log(sum);
+
+console.log('Booleans');
+let j = true;
+let h = false;
+console.log('It has been raining in past few days', j);
+console.log('Today is a sunny day', h);
+console.log('Comparison Operators');
+
+let a = 1;
+let b = 2;
+
+let c = a > b;
+console.log("Is a more than b?"+ c);
+let d = a < b;
+console.log("Is a less than b?"+ d);
+let e = a === b;
+console.log("Is a equal to b?"+ e);
+let f = a != b;
+console.log ("Is not equal to b?"+ f);
